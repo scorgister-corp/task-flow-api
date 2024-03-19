4a5657c41da6aa539a030d42e0217547981b3dbc
Django-Argus <malaska@mailo.com>
2024-03-19 14:41:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const core = require('./taskflow_core')
 const logger = require('./logger');
 
 const log = logger("Server");
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 const VERSION = "1.0"
+const BAD_CREDENTIALS = 0;
 
 
 // -- WITHOUT TOKEN -- \\
@@ -16,6 +18,40 @@ app.get("/version", (req, res) => {
     res.json({version: VERSION})
 });
 
+app.post("/register", (req, res) => {
+    if(!req.body["username"] || !req.body["password"] || !req.body["email"]) {
+        send401(res);
+        return;
+    }
+   
+    var username = req.body["username"];
+    var password = req.body["password"];
+    var email    = req.body["email"];
+    var result   = core.createAccount(username, password, email);
+   
+    send(res, {result: result});
+});
+   
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    if(req.body["username"] == undefined || req.body["password"] == undefined) {
+        send400(res);
+        return;
+    }
+   
+    var username = req.body["username"];
+    var password = req.body["password"];
+   
+    var result = core.getTokenFromAccountInfo(username, password);
+    if(!result) {
+        send(res, {connection: false, error: BAD_CREDENTIALS})
+        return;
+    }
+   
+    send(res, {connection: true, token: result});
+});
+   
+
 
 // verify token
 app.all("*", (req, res) => {
@@ -23,6 +59,7 @@ app.all("*", (req, res) => {
         send401(res);
         return;
     }
+
     // vérifier le token 
     //si faux alors send 401
     req.next();
@@ -30,37 +67,16 @@ app.all("*", (req, res) => {
 
 // ---- WITH TOKEN ---- \\
 
-app.post("/register", (req, res) => {
- if(!req.body["username"] || !req.body["password"] || !req.body["email"]) {
-        res.sendStatus(401);
-        return;
-    }
 
-    var username = req.body["username"];
-    var password = req.body["password"];
-    var email    = req.body["email"];
-
-    // creer l'utilisateur
-    res.json({result: true});
-});
-
-app.post("/login", (req, res) => {
-    if(!req.body["username"] || !req.body["password"]) {
-        res.sendStatus(401);
-        return;
-    }
-
-    var username = req.body["username"];
-    var password = req.body["password"];
-
-    // tenter la connection 
-    res.json({connection: true, token: ""});
-});
 
 // send 404
 app.all("*", (req, res) => {
     send404(res);
 })
+
+function send400(res) {
+    sendError(res, "Bad Request", 400);
+}
 
 function send401(res) {
     sendError(res, "Unauthorized", 401);
@@ -78,9 +94,15 @@ function sendError(res, msg, code) {
     send(res, {error: code, message: msg}, code);
 }
 
+/**
+ * 
+ * @param {express.Response} res 
+ * @param {JSON} body 
+ * @param {int} code 
+ */
 function send(res, body, code=200) {
     res.status(code);
-    res.send(body);
+    res.json(body);
 }
 
 module.exports.start = (port=8100) => {
